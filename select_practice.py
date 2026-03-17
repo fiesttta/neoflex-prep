99db0de43133fda107d461ff58cc21d1074442b3
fiestta <fiestttttta@gmail.com>
2026-03-17 21:07:34 +0400

    Добавил фильтрацию WHERE и улучшил вывод

--- select_practice.py
+++ select_practice.py
@@ -5,15 +5,15 @@ connection = sqlite3.connect('store.db')
 cursor = connection.cursor()
 
 # Запрос на выборку всех данных
-print("--- Все видеокарты на складе ---")
-cursor.execute("SELECT * FROM gpus")
+print("--- Видеокарты на складе (в наличии) ---")
+cursor.execute("SELECT * FROM gpus WHERE in_stock = 1")
 
 # Перехват результатов с курсора в переменную
 all_gpus = cursor.fetchall()
 
 # Проходимся по списку и печатаем
 for gpu in all_gpus:
-    print(gpu)
+    print(f"[{gpu[0]}] Видеокарта: {gpu[1]} | Цена: {gpu[2]} руб.")
 
 # закрытие соединения
 connection.close()
